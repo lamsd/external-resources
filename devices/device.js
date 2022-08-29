0ab81a65bf19c1c13dcc4111d123687bf11bec85
lamsd <huynhlam14091994@gmail.com>
2022-08-30 06:32:53 +0700

    Uploading first

---
+++ devices/device.js
@@ -0,0 +1,18 @@
+module.exports = [
+    // Buid-in device
+    'arduinoUno',
+    'arduinoNano_arduinoUno',
+    'arduinoMini_arduinoUno',
+    'arduinoLeonardo',
+    'arduinoMega2560',
+    'arduinoEsp32',
+    'arduinoEsp8266',
+    'microbit',
+    'microbitV2',
+    'makeyMakey',
+    // Third party
+    'ironKit_arduinoUno',
+    'QDPRobot_arduinoUnoUltra',
+    'QDPRobotC02_arduinoEsp32',
+    'arduinoAkid'
+];
